e7e33022f899ac4fb89ab8c430a423b4841a17b8
AfilAhamed <afilahamed0007@gmail.com>
2023-12-08 14:57:35 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:budget_tracker/firebase_options.dart';
+import 'package:budget_tracker/view/home_screen/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Budget Tracker',
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
