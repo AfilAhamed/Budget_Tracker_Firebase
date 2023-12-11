d9cc2530703079d5d62d235af4574bc4e1b19a9e
AfilAhamed <afilahamed0007@gmail.com>
2023-12-11 21:49:29 +0530

    Auth Completed

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:budget_tracker/view/home_screen/widget/transactions_cards.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -12,13 +13,155 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         backgroundColor: Colors.blue.shade900,
-        // leading: IconButton(
-        //     onPressed: () {
-        //       logout();
+      ),
+      body: Column(
+        children: [
+          //--
+          const MainCard(),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                const Text(
+                  "Recent Transactions",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                ),
+                const Spacer(),
+                TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'View All',
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                    ))
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              offset: const Offset(0, 10),
+                              color: Colors.grey.withOpacity(0.09),
+                              blurRadius: 10.0,
+                              spreadRadius: 4.0,
+                            )
+                          ],
+                          borderRadius: BorderRadius.circular(15)),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        child: ListTile(
+                          title: Row(
+                            children: [
+                              Expanded(child: Text('Food Expense')),
+                              Text(
+                                '₹ 8000',
+                                style: TextStyle(color: Colors.green),
+                              ),
+                            ],
+                          ),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text('Balance',
+                                      style: TextStyle(
+                                          color: Colors.grey, fontSize: 14)),
+                                  Spacer(),
+                                  Text(
+                                    '₹ 500',
+                                    style: TextStyle(
+                                        color: Colors.grey, fontSize: 14),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                '25 oct 4.55 PM',
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
 
-        //     },
-        //     icon: const Icon(Icons.logout)),
+class MainCard extends StatelessWidget {
+  const MainCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue.shade900,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                ),
+                Text(
+                  '₹ 582000',
+                  style: TextStyle(
+                      fontSize: 44,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding:
+                const EdgeInsets.only(top: 30, bottom: 10, left: 10, right: 10),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+              color: Colors.white,
+            ),
+            child: const Row(
+              children: [
+                TransactionsCard(
+                  color: Colors.green,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                TransactionsCard(
+                  color: Colors.red,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/view/home_screen/widget/transactions_cards.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class TransactionsCard extends StatelessWidget {
+  const TransactionsCard({
+    super.key,
+    required this.color,
+  });
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+            color: color.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(10)),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Credit',
+                    style: TextStyle(color: color, fontSize: 14),
+                  ),
+                  Text(
+                    '₹ 5850',
+                    style: TextStyle(
+                        color: color,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(
+                  Icons.arrow_upward_outlined,
+                  color: color,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
